b06685e3183c909742b5a8d19674281de37195f1
n-gist <58081918+n-gist@users.noreply.github.com>
2026-02-02 06:24:26 +0500

    remove problemMatchers applyTo property

--- src/vs/workbench/contrib/tasks/common/problemCollectors.ts
+++ src/vs/workbench/contrib/tasks/common/problemCollectors.ts
@@ -10,7 +10,7 @@ import { IDisposable, DisposableStore, Disposable } from '../../../../base/commo
 
 import { IModelService } from '../../../../editor/common/services/model.js';
 
-import { ILineMatcher, createLineMatcher, ProblemMatcher, IProblemMatch, ApplyToKind, IWatchingPattern, getResource } from './problemMatcher.js';
+import { ILineMatcher, createLineMatcher, ProblemMatcher, IProblemMatch, IWatchingPattern, getResource } from './problemMatcher.js';
 import { IMarkerService, IMarkerData, MarkerSeverity, IMarker } from '../../../../platform/markers/common/markers.js';
 import { generateUuid } from '../../../../base/common/uuid.js';
 import { IFileService } from '../../../../platform/files/common/files.js';
@@ -47,8 +47,7 @@ export abstract class AbstractProblemCollector extends Disposable implements IDi
 	protected readonly modelListeners = new DisposableStore();
 	private tail: Promise<void> | undefined;
 
-	// [owner] -> ApplyToKind
-	protected applyToByOwner: Map<string, ApplyToKind>;
+	protected readonly matchersOwners: Set<string>;
 	// [owner] -> [resource] -> URI
 	private resourcesToClean: Map<string, Map<string, URI>>;
 	// [owner] -> [resource] -> [markerkey] -> markerData
@@ -87,16 +86,11 @@ export abstract class AbstractProblemCollector extends Disposable implements IDi
 		this.activeMatcher = null;
 		this._numberOfMatches = 0;
 		this._maxMarkerSeverity = undefined;
-		this.openModels = Object.create(null);
-		this.applyToByOwner = new Map<string, ApplyToKind>();
+		this.matchersOwners = new Set();
 		for (const problemMatcher of problemMatchers) {
-			const current = this.applyToByOwner.get(problemMatcher.owner);
-			if (current === undefined) {
-				this.applyToByOwner.set(problemMatcher.owner, problemMatcher.applyTo);
-			} else {
-				this.applyToByOwner.set(problemMatcher.owner, this.mergeApplyTo(current, problemMatcher.applyTo));
-			}
+			this.matchersOwners.add(problemMatcher.owner);
 		}
+		this.openModels = Object.create(null);
 		this.resourcesToClean = new Map<string, Map<string, URI>>();
 		this.markers = new Map<string, Map<string, Map<string, IMarkerData>>>();
 		this.deliveredMarkers = new Map<string, Map<string, number>>();
@@ -167,26 +161,6 @@ export abstract class AbstractProblemCollector extends Disposable implements IDi
 			this.clearBuffer();
 		}
 		return result;
-	}
-
-	protected async shouldApplyMatch(result: IProblemMatch): Promise<boolean> {
-		switch (result.description.applyTo) {
-			case ApplyToKind.allDocuments:
-				return true;
-			case ApplyToKind.openDocuments:
-				return !!this.openModels[(await result.resource).toString()];
-			case ApplyToKind.closedDocuments:
-				return !this.openModels[(await result.resource).toString()];
-			default:
-				return true;
-		}
-	}
-
-	private mergeApplyTo(current: ApplyToKind, value: ApplyToKind): ApplyToKind {
-		if (current === value || current === ApplyToKind.allDocuments) {
-			return current;
-		}
-		return ApplyToKind.allDocuments;
 	}
 
 	private tryMatchers(): IProblemMatch | null {
@@ -263,17 +237,7 @@ export abstract class AbstractProblemCollector extends Disposable implements IDi
 	}
 
 	private _cleanMarkers(owner: string, toClean: Map<string, URI>): void {
-		const uris: URI[] = [];
-		const applyTo = this.applyToByOwner.get(owner);
-		toClean.forEach((uri, uriAsString) => {
-			if (
-				applyTo === ApplyToKind.allDocuments ||
-				(applyTo === ApplyToKind.openDocuments && this.openModels[uriAsString]) ||
-				(applyTo === ApplyToKind.closedDocuments && !this.openModels[uriAsString])
-			) {
-				uris.push(uri);
-			}
-		});
+		const uris = Array.from(toClean.values());
 		this.markerService.remove(owner, uris);
 	}
 
@@ -388,16 +352,13 @@ export class StartStopProblemCollector extends AbstractProblemCollector implemen
 		const resource = await markerMatch.resource;
 		const resourceAsString = resource.toString();
 		this.removeResourceToClean(owner, resourceAsString);
-		const shouldApplyMatch = await this.shouldApplyMatch(markerMatch);
-		if (shouldApplyMatch) {
-			this.recordMarker(markerMatch.marker, owner, resourceAsString);
-			if (this.currentOwner !== owner || this.currentResource !== resourceAsString) {
-				if (this.currentOwner && this.currentResource) {
-					this.deliverMarkersPerOwnerAndResource(this.currentOwner, this.currentResource);
-				}
-				this.currentOwner = owner;
-				this.currentResource = resourceAsString;
+		this.recordMarker(markerMatch.marker, owner, resourceAsString);
+		if (this.currentOwner !== owner || this.currentResource !== resourceAsString) {
+			if (this.currentOwner && this.currentResource) {
+				this.deliverMarkersPerOwnerAndResource(this.currentOwner, this.currentResource);
 			}
+			this.currentOwner = owner;
+			this.currentResource = resourceAsString;
 		}
 	}
 }
@@ -492,14 +453,11 @@ export class WatchingProblemCollector extends AbstractProblemCollector implement
 		const owner = markerMatch.description.owner;
 		const resourceAsString = resource.toString();
 		this.removeResourceToClean(owner, resourceAsString);
-		const shouldApplyMatch = await this.shouldApplyMatch(markerMatch);
-		if (shouldApplyMatch) {
-			this.recordMarker(markerMatch.marker, owner, resourceAsString);
-			if (this.currentOwner !== owner || this.currentResource !== resourceAsString) {
-				this.reportMarkersForCurrentResource();
-				this.currentOwner = owner;
-				this.currentResource = resourceAsString;
-			}
+		this.recordMarker(markerMatch.marker, owner, resourceAsString);
+		if (this.currentOwner !== owner || this.currentResource !== resourceAsString) {
+			this.reportMarkersForCurrentResource();
+			this.currentOwner = owner;
+			this.currentResource = resourceAsString;
 		}
 	}
 
@@ -573,7 +531,7 @@ export class WatchingProblemCollector extends AbstractProblemCollector implement
 	}
 
 	public override done(): void {
-		[...this.applyToByOwner.keys()].forEach(owner => {
+		this.matchersOwners.forEach(owner => {
 			this.recordResourcesToClean(owner);
 		});
 		super.done();
